5ac5ab41aedf5ea32139ad069c5a33d275d423ba
Walterhart <70658679+Walterhart@users.noreply.github.com>
2023-01-06 04:14:46 -0500

    Display file directory, filename and display hello world per second

---
+++ 1-simple-output.js
@@ -0,0 +1,12 @@
+// Simple node output
+
+const amount = 12
+
+if(amount < 10) {
+    console.log('small number');
+}
+else{
+    console.log('large number')
+}
+
+console.log('node js_test')
--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-const  amount =9
 
-if(amount < 10) {
-    console.log('small number');
-}
-else{
-    console.log('large number')
-}
+// Display current directory and file
+console.log(__dirname)
+console.log(__filename)
 
-console.log('node js_test')
+// Print hello world per sec
+setInterval(() =>{
+    console.log('hello world')
+}, 1000)
