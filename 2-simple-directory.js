2015b938f9316d977303523c3b531fa6b8a385f7
Walterhart <70658679+Walterhart@users.noreply.github.com>
2023-01-07 02:51:02 -0500

    Update 2-simple-directory.js

---
+++ 2-simple-directory.js
@@ -0,0 +1,9 @@
+
+// Display current directory and file
+console.log(__dirname)
+console.log(__filename)
+
+// Print hello world per sec
+setInterval(() =>{
+    console.log('hello world')
+}, 1000)
