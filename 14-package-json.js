45077ead7731ef9b01894cc124a96a56ce30a874
walterhart <walterhart1996@gmail.com>
2023-01-08 15:15:38 -0500

    Test use of a simple module from npm

---
+++ 14-package-json.js
@@ -0,0 +1,20 @@
+// npm has many modules that can be downloaded
+
+// local dependency - use  on certain project
+// npm i <packageName>
+
+// npm install -g <packageName> - use in any project
+// for mac : sudo npm install -g <packageName>
+
+// package.json - mainifest file (stores info about project/package)
+// 3 ways to create package.json
+
+// manual (create package.json in root)
+// npm init(step-by-step, press enter to skip)
+// npm init -y (everything default)
+
+const _ = require('lodash')
+
+const items = [1,[2, [3, [4]]]]
+const newItems = _.flattenDeep(items)
+console.log(newItems)
