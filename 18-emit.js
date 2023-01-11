2172b7934b260d9bd339cf872f523c5009780906
Walterhart <70658679+Walterhart@users.noreply.github.com>
2023-01-11 03:08:48 -0500

    Practice  event emitter

---
+++ 18-emit.js
@@ -0,0 +1,23 @@
+// Event Emitter
+// node js very Event-driven programming
+
+const EventEmitter = require('events');
+
+const customEmitter = new EventEmitter()
+
+// on(EventName): listen to event
+
+customEmitter.on('response', (name,id) => {
+    console.log(`data recieved ${name} with id: ${id}`)
+})
+
+customEmitter.on('response', () => {
+    console.log(`data recieved again`)
+})
+
+// emit : emit an event
+// can have many method of event
+// orders matter: emit afters on
+// can pass argument when emitting event
+// emit(on, argument,argument.....)
+customEmitter.emit('response', 'john', 34)
